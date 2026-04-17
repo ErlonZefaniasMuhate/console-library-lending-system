c7982e1d25084543203aec1de1cbe10d1febc037
emuhate <erlonzefaniasmuhate@gmail.com>
2026-04-17 15:05:43 +0200

    Implemented borrowBook() and returnBook()

--- src/main/java/com/emuhate/library/model/Member.java
+++ src/main/java/com/emuhate/library/model/Member.java
@@ -1,5 +1,6 @@
 package com.emuhate.library.model;
 
+import com.emuhate.library.exception.BookNotAvailableException;
 import com.emuhate.library.exception.MaxBooksExceededException;
 
 import static com.emuhate.library.resources.IDGenerator.generateUUID;
@@ -12,6 +13,7 @@ public class Member extends Person{
 
     {
         memberID = generateUUID(name);
+        borrowedBooks = new Book[MAX_BOOKS_ALLOWED];
     }
 
     public Member(String email, String name) {
@@ -23,9 +25,45 @@ public class Member extends Person{
         return "";
     }
 
-    public void borrowBook(Book book) throws MaxBooksExceededException {
+    public Book[] getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) throws BookNotAvailableException, MaxBooksExceededException {
+        boolean success = false;
+
+        for (int i = 0; i < MAX_BOOKS_ALLOWED; i++) {
+            if(this.borrowedBooks[i] == null){
+
+                book.checkout(this);
+
+                this.borrowedBooks[i] = book;
+                return;
+            }
+        }
+
+        if(!success){
+            throw new MaxBooksExceededException("Member cannot borrow more than 3 books.");
+        }
     }
 
     public void returnBook(Book book) {
+        boolean sucess = false;
+
+        for(int i = 0; i < MAX_BOOKS_ALLOWED; i++) {
+            if(book.getIsbn().equals(borrowedBooks[i].getIsbn())){
+                book.returnItem();
+                borrowedBooks[i] = null;
+                sucess = true;
+            }
+        }
+
+        if(!sucess){
+            throw new IllegalArgumentException("Member "
+                .concat(memberID)
+                .concat(" has not borrowed ")
+                .concat(book.getTitle())
+            );
+        }
     }
 }
