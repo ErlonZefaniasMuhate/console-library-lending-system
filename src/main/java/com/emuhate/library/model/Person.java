7326318a0cf36655fba67fb896fa34bcdaee88d8
emuhate <erlonzefaniasmuhate@gmail.com>
2026-04-17 15:06:29 +0200

    Added getters and setters

--- src/main/java/com/emuhate/library/model/Person.java
+++ src/main/java/com/emuhate/library/model/Person.java
@@ -11,4 +11,20 @@ public abstract class Person {
     }
 
     public abstract String getDescription();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
